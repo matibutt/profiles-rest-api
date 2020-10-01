53c291755bc68c46aceb3074db3a47316c243306
IamMati <email@gmail.com>
2020-10-01 05:34:22 +0200

    Added create retrieve update patch and destroy method to our viewset.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -56,6 +56,8 @@ class HelloApiView(APIView):
 class HelloViewSet(viewsets.ViewSet):
     """Test Api view sets."""
 
+    serializer_class= serializers.HelloSerializer
+
     def list(self, request):
         """Return a hello message"""
 
@@ -66,3 +68,35 @@ class HelloViewSet(viewsets.ViewSet):
         ]
 
         return Response ({'message':'Hello', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new hello message"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handles getting an object by its IDs"""
+
+        return Response({'Http_method':'Get'})
+
+    def update(self, request, pk=None):
+        """Handles updatin ab object"""
+
+        return Response({'Http_method':'Put'})
+
+    def patch(self, request, pk=None):
+        """Handles updation part of an object"""
+
+        return Response({'Http_method':'Patch'})
+
+    def destroy(self, request, pk=None):
+        """Handles removing an object"""
+
+        return Response({'Http_method':'Delete'})
